d9f693fb180a876aaf42a9b1ec6e38c802944dae
nicfigu <nicfigu@gmail.com>
2024-02-16 14:39:18 -0500

    try

--- service-worker.js
+++ service-worker.js
@@ -28,6 +28,6 @@ chrome.runtime.onInstalled.addListener(() => {
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'openSidePanel') {
     // This will open the panel in all the pages on the current window.
-    chrome.sidePanel.open({ windowId: tab.windowId });
+    chrome.sidePanel.open({ windowId });
   }
 });
