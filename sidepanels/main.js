ffe72fb4a51457a4b1df4bcfd89b1802ce01b812
nicfigu <nicfigu@gmail.com>
2024-02-17 16:40:56 -0500

    line rmv

--- sidepanels/main.js
+++ sidepanels/main.js
@@ -6,11 +6,8 @@ let tasks = [];
 const showTasks = () => {
 
     chrome.storage.sync.get(['todos']).then(data => {
-
         let html = '';
-
         if (data.todos != undefined && data.todos.length > 0) {
-
             data.todos.forEach((todo, index) => {
                 html += `<li class="list-group-item d-flex align-items-center">`;
                 html += `<input type="checkbox" class="form-check-input me-2" data-task-id="${index}" ${todo.completed ? 'checked' : ''}>`
@@ -23,12 +20,9 @@ const showTasks = () => {
             });
 
         } else {
-
             html = `<li class="list-group-item text-secondary text-center">No Tasks</li>`;
         }
-
         document.querySelector('ul#tasks').innerHTML = html;
-
     });
 }
 
@@ -39,33 +33,22 @@ showTasks();
 
 // add new tasks
 document.getElementById('add-task').addEventListener('submit', (e) => {
-
     e.preventDefault();
-
     let task = document.querySelector('input#task').value;
-
     document.querySelector('input#task').value = '';
-
     tasks.push({ completed: false, title: task });
-
     chrome.storage.sync.set({ todos: tasks }).then(showTasks);
 
 });
 
 
 document.querySelector('ul#tasks').addEventListener('click', async (e) => {
-
     // complete task
     if (e.target.getAttribute('data-task-id')) {
-
         let index = e.target.getAttribute('data-task-id');
-
         let data = await chrome.storage.sync.get(['todos']);
-
         tasks = data.todos;
-
         let task = tasks[index];
-
         e.target.checked
             ? tasks[index] = { completed: true, title: task.title }
             : tasks[index] = { completed: false, title: task.title };
